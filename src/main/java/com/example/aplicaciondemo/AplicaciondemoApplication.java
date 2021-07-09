dd2c6b10a112c7d75a6f0264dff7429826d85c8b
jeremias <eturajere8@gmail.com>
2021-07-09 12:05:33 -0500

    subiendo los primeros archivos

---
+++ src/main/java/com/example/aplicaciondemo/AplicaciondemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplicaciondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicaciondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicaciondemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aplicaciondemo/AplicaciondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplicaciondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicaciondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
